1a92626033025df8f930528a7f67f921e6cf6181
Charles Ment <cm1099@exeter.ac.uk>
2025-03-11 12:14:00 +0000

    Put SVGs into their own separate file

---
+++ bp-attacks/src/SVGAssets.js
@@ -0,0 +1,5 @@
+
+
+export const gatewaySVG = `<?xml version="1.0" encoding="UTF-8"?> <svg width="121.32mm" height="174.97mm" version="1.1" viewBox="0 0 121.32 174.97" xmlns="http://www.w3.org/2000/svg"> <path d="m0.4 0.4v26.364h31.071v121.44h-31.071v26.364h120.52v-26.364h-31.071v-121.44h31.071v-26.364z" fill="#fdcb6e" stroke="#000" stroke-width=".8"/> </svg>`;
+
+export const userTaskSVG = `<?xml version="1.0" encoding="UTF-8"?> <svg width="369.39mm" height="496.32mm" version="1.1" viewBox="0 0 205.22 275.73" xmlns="http://www.w3.org/2000/svg"> <g transform="translate(-2.2236 -20.838)"> <path d="m104.98 24.467a73.362 74.344 0 0 0-73.362 74.344 73.362 74.344 0 0 0 24.733 55.59 100.06 191.25 0 0 0-50.2 138.49c204.61 2e-3 198.33 0.0405 197.67 0.0425a100.06 191.25 0 0 0-50.145-138.6 73.362 74.344 0 0 0 24.662-55.517 73.362 74.344 0 0 0-73.362-74.344z" fill="#a29bfe" stroke="#000" stroke-width="7.2576"/> </g> </svg>`;
--- bp-attacks/src/Sketch.js
+++ bp-attacks/src/Sketch.js
@@ -8,7 +8,6 @@ import {KeyboardArrowDown, KeyboardArrowUp, PlayArrow, SkipNext, SkipPrevious, C
 import cytoscape from "cytoscape";
 import klay from "cytoscape-klay";
 
-import gatewaySVG from "./symbols/exclusive_gateway.svg";
 import gateway from "./symbols/gateway.png";
 import inputOutput from "./symbols/inputOutput.png";
 import inputOutputFault from "./symbols/inputOutputFault.png";
@@ -17,6 +16,8 @@ import eventFault from "./symbols/eventFault.png";
 import eventSymbol from "./symbols/event.png";
 import openIcon from "./symbols/openIcon.png";
 import closeIcon from "./symbols/closeIcon.png";
+
+import { gatewaySVG,  userTaskSVG} from './SVGAssets.js';
 
 
 import CodeBlock from './CodeBlock.js';
@@ -27,12 +28,8 @@ import NodeLabel from './NodeLabel.js';
 
 export default function Sketch() {
     
-   const gatewaySVG = `<?xml version="1.0" encoding="UTF-8"?>
-<svg width="121.32mm" height="174.97mm" version="1.1" viewBox="0 0 121.32 174.97" xmlns="http://www.w3.org/2000/svg">
-<path d="m0.4 0.4v26.364h31.071v121.44h-31.071v26.364h120.52v-26.364h-31.071v-121.44h31.071v-26.364z" fill="#fdcb6e" stroke="#000" stroke-width=".8"/>
-</svg>`;
+   
 
-    const userTaskSVG = `<?xml version="1.0" encoding="UTF-8"?> <svg width="369.39mm" height="496.32mm" version="1.1" viewBox="0 0 205.22 275.73" xmlns="http://www.w3.org/2000/svg"> <g transform="translate(-2.2236 -20.838)"> <path d="m104.98 24.467a73.362 74.344 0 0 0-73.362 74.344 73.362 74.344 0 0 0 24.733 55.59 100.06 191.25 0 0 0-50.2 138.49c204.61 2e-3 198.33 0.0405 197.67 0.0425a100.06 191.25 0 0 0-50.145-138.6 73.362 74.344 0 0 0 24.662-55.517 73.362 74.344 0 0 0-73.362-74.344z" fill="#a29bfe" stroke="#000" stroke-width="7.2576"/> </g> </svg>`;
 
     const event_types = [
         "endEvent",
