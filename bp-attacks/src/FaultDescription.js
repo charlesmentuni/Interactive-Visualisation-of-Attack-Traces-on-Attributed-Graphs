857aa1bd872fdfb636159f3d51de9721491d1e80
Charles Ment <cm1099@exeter.ac.uk>
2025-03-16 12:49:51 +0000

    Centred text on fault description

--- bp-attacks/src/FaultDescription.js
+++ bp-attacks/src/FaultDescription.js
@@ -1,4 +1,4 @@
-import { Card, CardContent } from "@mui/material";
+import { Card, CardContent, Typography } from "@mui/material";
 import { useContext } from "react";
 import { FaultContext } from "./Sketch";
 
@@ -8,25 +8,59 @@ export default function FaultDescription() {
     const {fault, setFault, fault_dict} = useContext(FaultContext);
     return (
         <>
-        <Card sx={{
-            position: 'absolute', 
-            opacity: 0.75,
-            margin: '2%',
-            bottom:0,
-            height: '20%',
-            width: '64%', 
-            backgroundColor: 'black', 
-            color: '#fefefe',
-            overflow: 'auto',
-            padding:0,
-        }}>
-            <CardContent sx={{color: 'white', fontFamily: 'monospace', padding:0, paddingLeft:'2ch'}}>
-            {fault_dict[fault] ? Object.keys(fault_dict[fault]).map((key) => {
-                    return <p>{key}: {fault_dict[fault][key].toString()}</p>
-                }) : null}
-            </CardContent>
+        <Card
+  sx={{
+    position: "absolute",
+    opacity: 0.75,
+    margin: "2%",
+    bottom: 0,
+    height: "20%",
+    width: "64%",
+    backgroundColor: "black",
+    color: "#fefefe",
+    overflow: "auto",
+    display: "flex",
+  }}
+>
+  <CardContent
+  paddingBottom={0}
+    sx={{
+      color: "white",
+      fontFamily: "monospace",
+      padding: 0,
+      width: "100%",
+      height: "100%",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: fault_dict[fault] ? "flex-start" : "center",
+      alignItems: fault_dict[fault] ? "flex-start" : "center",
+      overflowY: fault_dict[fault] ? "auto" : "hidden",
+      paddingTop: fault_dict[fault] ? "1rem": "0", 
+      paddingBottom: '0 !important',
+    }}
+  >
+    {fault_dict[fault] ? (
+      Object.keys(fault_dict[fault]).map((key) => (
+        <Typography
+          key={key}
+          variant="p"
+          sx={{
+            fontFamily: "monospace",
+            paddingLeft: "2ch",
+            marginBottom: "0.5rem", 
+          }}
+        >
+          {key}: {fault_dict[fault][key].toString()}
+        </Typography>
+      ))
+    ) : (
+      <Typography variant="h3" sx={{ textAlign: "center" }}>
+        No fault selected
+      </Typography>
+    )}
+  </CardContent>
+</Card>
 
-        </Card>
             
 
         </>
--- bp-attacks/src/NodeLabel.js
+++ bp-attacks/src/NodeLabel.js
@@ -5,14 +5,14 @@ import {Card, CardContent} from '@mui/material';
 export default function NodeLabel({node}) {
     return (
         <Card sx={node &&{
-    position: 'absolute', 
-    top: node.position.y*100+'%', 
-    left: node.position.x*100+'vw', 
-    margin: '2%', 
-    width: '16%', 
-    backgroundColor: '#000000', 
-    color: '#fefefe'
-    }}>
+            position: 'absolute', 
+            top: node.position.y*100+'%', 
+            left: node.position.x*100+'vw', 
+            margin: '2%', 
+            width: '16%', 
+            backgroundColor: '#000000', 
+            color: '#fefefe'
+            }}>
         <CardContent>
             {node ? node.name : "No Node Selected"}
         </CardContent>
